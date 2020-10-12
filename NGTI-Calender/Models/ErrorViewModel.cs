e57a4b66467bf911ca7a752ec6264663fded83f6
Kokosnootje <judacat@hotmail.com>
2020-10-12 10:46:17 +0200

    Add project files.

---
+++ NGTI-Calender/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NGTI_Calender.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
