c065778819a1903173238317f0375f5f812b7834
Thomas Vos <Tvvos@hotmail.nl>
2020-10-13 14:41:45 +0200

    Models for person and reservation made Migrations for person and reservation made Views (CRUD) for person and reservation made

---
+++ NGTI-Calender/Models/Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGTI_Calender.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+
+        // public Office office {get; set;}
+        public Person Person { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
