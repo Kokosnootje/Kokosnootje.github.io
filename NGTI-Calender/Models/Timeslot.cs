5ea8e50eda9d106cea81ad807a6f6c0c2c2d9069
Thomas Vos <Tvvos@hotmail.nl>
2020-10-26 13:38:59 +0100

    Timeslot model, controller en views toegevoegd. Timeslot ook toegevoegd aan Reservation

---
+++ NGTI-Calender/Models/Timeslot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGTI_Calender.Models
+{
+    public class Timeslot
+    {
+        public int TimeslotId { get; set; }
+        public string TimeStart { get; set; }
+        public string TimeEnd { get; set; }
+    }
+}
