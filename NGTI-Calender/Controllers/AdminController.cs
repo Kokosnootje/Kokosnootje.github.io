60c5e4c71dc1ff8fe4f528e59016ccb4d9db78d0
Ben <0984798@hr.nl>
2020-11-02 13:21:26 +0100

    taak 5a
    -admin view en controller gemaakt
    -nav code gefixt

---
+++ NGTI-Calender/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NGTI_Calender.Data;
+
+namespace NGTI_Calender.Controllers {
+    public class AdminController : Controller {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IActionResult Index() {
+            return View(_context.Timeslot.ToArray());
+        }
+    }
+}
