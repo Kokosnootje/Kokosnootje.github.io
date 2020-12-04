f5ff058b04bfe15596be55eda1ac4205f5c3f6a2
Lucavdberg <lucaberg123@gmail.com>
2020-12-04 16:50:37 +0100

    eigen reserveringen verwijderen
    gebruiken kan nu zijn/haar eigen reserveringen verwijderen

--- NGTI-Calender/Controllers/OverviewController.cs
+++ NGTI-Calender/Controllers/OverviewController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NGTI_Calender.Data;
+using NGTI_Calender.Models;
 
 namespace NGTI_Calender.Controllers {
     public class OverviewController : Controller {
@@ -15,23 +16,29 @@ namespace NGTI_Calender.Controllers {
 
         // GET: Overview/Index
         public IActionResult Index(string personId) {
-            var tuple = Tuple.Create(_context.Timeslot.ToList(), personId, _context.Reservation.ToList(), _context.Person.ToList());
+            var tuple = Tuple.Create(_context.Timeslot.ToList(), personId, _context.Reservation.ToList(), _context.Person.ToList(), new Reservation());
             return View(tuple);
         }
 
         // POST: Overview/Index
         [HttpPost, ActionName("Index")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Delete(string resList)
+        public async Task<IActionResult> Delete(string resList, string personId)
         {
+            string p = personId;
             string[] resids = resList.Split(' ');
-            //for(int i = 0; i < resids.Length; i++)
-            //{
-             //   var reservation = await _context.Reservation.FindAsync(resids[i]);
-               // _context.Reservation.Remove(reservation);
-                //await _context.SaveChangesAsync();
-            //}
-            return RedirectToAction(nameof(Index));
+            int[] reservationIds = new int[resids.Length];
+            for(int j = 0; j < resids.Length; j++)
+            {
+                reservationIds[j] = Int32.Parse(resids[j]);
+            }
+            for(int i = 0; i < resids.Length; i++)
+            {
+               var reservation = await _context.Reservation.FindAsync(reservationIds[i]);
+               _context.Reservation.Remove(reservation);
+               await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", new { personId = personId});
         }
     }
 }
